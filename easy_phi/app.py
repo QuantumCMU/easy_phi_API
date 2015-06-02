3463d09c15a2c2dfa6076cb556b5683c464d88ff
rezniknn <rezniknn@gmail.com>
2015-06-02 18:52:10 +0300

    @format decorator added

--- easy_phi/app.py
+++ easy_phi/app.py
@@ -3,6 +3,7 @@
 """
 This file contains Web Application
 """
+import json
 
 import tornado.ioloop
 import tornado.web
@@ -11,26 +12,42 @@ from datetime import date
 VERSION = "0.1"
 LICENSE = "GPL v3.0"
 PROJECT = "easy_phi"
-SERVER_PORT = "easy_phi"
+SERVER_PORT = 8888
+
+
+def toXML(res):
+    #TODO convert String to XML representation
+    return res
+
+
+def format(func):
+    def wrapper(self):
+        ct = self.request.headers.get('accept')
+        res = func(self)
+        if "application/json" in ct:
+            self.write(json.dumps(res))
+        elif "application/xml" in ct:
+            self.write(toXML(res))
+        else:
+            self.write(res)
+    return wrapper
 
 class VersionHandler(tornado.web.RequestHandler):
     """
     Return Rack software verion and last release date
     """
-
+    @format
     def get(self):
         response = {'version': VERSION,
                     'last_build': date.today().isoformat()}
-        self.write(response)
+        return response
 
 
 class ModulesListHandler(tornado.web.RequestHandler):
     """
     ModulesListHandler
-
     Return a list of modules currently connected to the Rack
     with their information
-
     Module information includes:
     - Module ID
     - Module Description
@@ -38,17 +55,15 @@ class ModulesListHandler(tornado.web.RequestHandler):
     - Current status: locked/unlocked
     - List of supported SCPI commands
 """
-
+    @format
     def get(self):
-        response = {'module3': 'module3_info',
-                    'module2': 'module2_info',
-                    'module1': 'module1_info'}
-        self.write(response)
-
+        response = {'Slot1': 'Module1',
+                    'Slot2': 'Module2'}
+        return response
 
 class SelectModuleHandler(tornado.web.RequestHandler):
     """Select the module, identified by ID passed as a URL parameter."""
-
+    @format
     def post(self):
         # Parse request URL and derive moduleID [TBD]
 
@@ -59,18 +74,20 @@ class SelectModuleHandler(tornado.web.RequestHandler):
         # Redirect user to a constructed Module specific web-page [TBD]
 
         response = {'handler': 'SelectModuleHandler'}
-        self.write(response)
+        return response
 
+    @format
     def get(self):
         # Debug purpose only
 
         response = {'handler': 'SelectModuleHandler'}
-        self.write(response)
+        return response
 
 
 class SCPICommandHandler(tornado.web.RequestHandler):
     """Transfer SCPI command to a module, specified by moduleID"""
 
+    @format
     def post(self):
         # Parse request URL and derive moduleID [TBD]
 
@@ -78,29 +95,32 @@ class SCPICommandHandler(tornado.web.RequestHandler):
 
         # Update web-page accordingle [TBD]
         response = {'handler': 'SCPICommandHandler'}
-        self.write(response)
+        return response
 
+    @format
     def get(self):
         # Debug purpose only
 
         response = {'handler': 'SCPICommandHandler'}
-        self.write(response)
+        return response
 
 
 class AdminConsoleHandler(tornado.web.RequestHandler):
     """Redirect user to Admin Console web-page"""
 
+    @format
     def post(self):
         # Redirect user to Admin Console web-page [TBD]
 
         response = {'handler': 'AdminConsoleHandler'}
-        self.write(response)
+        return response
 
+    @format
     def get(self):
         # Debug purpose only
 
         response = {'handler': 'AdminConsoleHandler'}
-        self.write(response)
+        return response
 
 # URL schemas to RequestHandler classes mapping
 APPLICATION = tornado.web.Application([
