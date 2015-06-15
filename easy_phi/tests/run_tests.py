8bc640447f15ebb42b4bc0b45144cd59b35cb2d1
mansuleman <mansuleman@gmail.com>
2015-06-15 11:43:01 +0300

    update tests

--- easy_phi/tests/run_tests.py
+++ easy_phi/tests/run_tests.py
@@ -11,30 +11,32 @@ class API_test(unittest.TestCase):
         self.assertTrue(True)
 
 class TestTornadoWeb(unittest.TestCase):
+    http_server = None
     response = None
 
     def setUp(self):
         application = tornado.web.Application([
-                (r'/', VersionHandler),
+                (r'/api/v1/', PlatformInfoHandler),
                 ])
 
         http_server = tornado.httpserver.HTTPServer(application)
-        http_server.listen(8888)
+        http_server.listen(8000)
+    def tearDown(self):
+	self.http_server.stop()
 
     def handle_request(self, response):
         self.response = response
-
         tornado.ioloop.IOLoop.instance().stop()
 
-    def testVersionHandler(self):
+    def testPlatformInfoHandler(self):
         http_client = tornado.httpclient.AsyncHTTPClient()
-        http_client.fetch('http://localhost:8888/version',
+        http_client.fetch('http://localhost:8000/api/v1/',
 self.handle_request)
 
         tornado.ioloop.IOLoop.instance().start()
 
-        self.failIf(self.response.error)
-        self.assertEqual('success', self.response.body)
+        self.failIf(self.responsse.error)
+        self.assertEqual( self.response.body, "success")
 
 
 if __name__ == '__main__':
