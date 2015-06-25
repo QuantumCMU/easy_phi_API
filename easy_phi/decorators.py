0fcf3a3ef0ac55253dbd9dad3d17657c898675c5
Marat <valiev.m@gmail.com>
2015-06-25 23:28:14 +0300

    format unittest and fixed plaintext formatting

--- easy_phi/decorators.py
+++ easy_phi/decorators.py
@@ -3,48 +3,61 @@
 
 import json
 import dicttoxml
+from xml.dom.minidom import parseString
 
 from tornado.options import options, define
 
 import hwconf
 import auth
 
-define("default_format", default='json')
+DEFAULT_FORMAT = 'json'
+define("default_format", default=DEFAULT_FORMAT)
+
+
+def format_conversion(res, fmt=DEFAULT_FORMAT, callback=''):
+    if fmt == 'xml':
+        ctype = 'application/xml'
+        response_text = dicttoxml.dicttoxml(res)
+        if options.debug:  # prettify XML for better debugging
+            response_text = parseString(response_text).toprettyxml()
+
+    elif fmt == 'plain':  # Plain text
+        ctype = 'text/plain'
+        if isinstance(res, dict):
+            response_text = "\n".join((u"{0}: {1}".format(*item)
+                                       for item in res.items()))
+        elif isinstance(res, list):
+            response_text = "\n".join(map(unicode, res))
+        else:
+            response_text = unicode(res)
+    else:  # fmt == 'json':
+        json_kwargs = {
+            'sort_keys': True,
+            'indent': 4,
+            'separators': (',', ': ')
+        } if options.debug else {}
+
+        ctype = 'application/json'
+        response_text = json.dumps(res, **json_kwargs)
+        if callback:  # JSONP support, for cross-domain static JS API
+            response_text = ''.join((callback, '(', response_text, ')'))
+
+    return response_text, ctype
 
 
 def multiformat(func):
     """ Decorator for Tornado web request handlers to format response
     into xml/json/plain by the GET['format'] parameter
     """
-    if options.debug:  # pretty print support for xml output
-        from xml.dom.minidom import parseString
 
     def wrapper(self):
         fmt = self.get_argument('format', options.default_format)
         res = func(self)
-        if fmt == 'json':
-            callback = self.get_argument('callback', '')
-            json_kwargs = {
-                'sort_keys': True,
-                'indent': 4,
-                'separators': (',', ': ')
-            } if options.debug else {}
-
-            self.set_header('content-type', 'application/json')
-            response_text = json.dumps(res, **json_kwargs)
-            if callback:  # JSONP support, for cross-domain static JS API
-                response_text = ''.join((callback, '(', response_text, ')'))
-
-        elif fmt == 'xml':
-            self.set_header('content-type', 'application/xml')
-            response_text = dicttoxml.dicttoxml(res)
-            if options.debug:  # prettify XML for better debugging
-                response_text = parseString(response_text).toprettyxml()
-
-        else:
-            self.set_header('content-type', 'text/plain')
-            response_text = unicode(res)
-
+        if res is None:  # generic handler, using self.write()
+            return
+        response_text, ctype = format_conversion(
+            res, fmt, callback=self.get_argument('callback', ''))
+        self.set_header('content-type', ctype)
         self.finish(response_text)
 
     return wrapper
@@ -83,7 +96,7 @@ def require_slot(allow_bcast=True):
                     else:
                         max_slot = len(hwconf.modules) - 1
                         min_slot = 0 if allow_bcast else 1
-                        if not min_slot <= self.slot <= max_slot:  # invalid slot number
+                        if not min_slot <= self.slot <= max_slot:
                             err = 'Invalid slot number. Number in range ' + \
                                   '{0}..{1} expected'.format(min_slot, max_slot)
                         elif hwconf.modules[self.slot] is None:
---
+++ easy_phi/tests/decorators_test.py
@@ -0,0 +1,81 @@
+# -*- coding: utf-8 -*-
+
+from tornado.test.util import unittest
+from tornado.options import options
+
+from easy_phi import decorators
+
+
+class DecoratorsTest(unittest.TestCase):
+
+    def setUp(self):
+        # necessary to have repeatable results - no pretty print for xml/json
+        options.debug = False
+
+    def test_format_conversion_string(self):
+
+        response_text, ctype = \
+            decorators.format_conversion("Hello world", 'plain')
+        self.assertEqual(u'Hello world', response_text)
+
+        response_text, ctype = \
+            decorators.format_conversion("Hello world", 'xml')
+        self.assertEqual('<?xml version="1.0" encoding="UTF-8" ?><root>'
+                         '<item type="str">Hello world</item></root>',
+                         response_text)
+
+        response_text, ctype = \
+            decorators.format_conversion("Hello world", 'json')
+        self.assertEqual('"Hello world"', response_text)
+
+    def test_format_conversion_list(self):
+        response_text, ctype = \
+            decorators.format_conversion(['one', 1, None], 'plain')
+        self.assertEqual(response_text, "one\n1\nNone")
+
+        response_text, ctype = \
+            decorators.format_conversion(['one', 1, None], 'xml')
+        self.assertEqual('<?xml version="1.0" encoding="UTF-8" ?><root>'
+                         '<item type="str">one</item><item type="int">1</item>'
+                         '<item type="null"></item></root>', response_text)
+
+        response_text, ctype = \
+            decorators.format_conversion(['one', 1, None], 'json')
+        self.assertEqual('["one", 1, null]', response_text)
+
+    def test_format_conversion_dict(self):
+        # Since dict is not ordered, in this test we have to test
+        # both possible orders of keys
+        response_text, ctype = \
+            decorators.format_conversion({'one': 1, 2: None}, 'plain')
+        self.assertTrue("one: 1\n2: None" == response_text or
+                        "2: None\none: 1" == response_text)
+
+        # dicttoxml does not support integer indices
+        response_text, ctype = \
+            decorators.format_conversion({'one': 1, '45': None}, 'xml')
+        self.assertTrue('<?xml version="1.0" encoding="UTF-8" ?><root>'
+                        '<n45 type="null"></n45><one type="int">1</one>'
+                        '</root>' == response_text or
+                        '<?xml version="1.0" encoding="UTF-8" ?><root>'
+                        '<one type="int">1</one><n45 type="null"></n45>'
+                        '</root>' == response_text)
+
+        response_text, ctype = \
+            decorators.format_conversion({'one': 1, 2: None}, 'json')
+        self.assertTrue('{"2": null, "one": 1}' == response_text or
+                        '{"one": 1, "2": null}' == response_text)
+
+    def test_format_conversion_ctype(self):
+
+        response_text, ctype = \
+            decorators.format_conversion("Hello world", 'plain')
+        self.assertEqual("text/plain", ctype)
+
+        response_text, ctype = \
+            decorators.format_conversion("Hello world", 'xml')
+        self.assertEqual("application/xml", ctype)
+
+        response_text, ctype = \
+            decorators.format_conversion("Hello world", 'json')
+        self.assertEqual("application/json", ctype)
