6fdb6088fc4a1955097205cf85354878c213de01
Marat <valiev.m@gmail.com>
2015-06-29 12:02:26 +0300

    UI update draft
    known issues - $.getScript doesn't fire

--- static/js/main.js
+++ static/js/main.js
@@ -5,167 +5,166 @@ $(document).ready(function () {
 });
 
 var ep = ep || {
+    slots: 0,
+    _username: 'Somebody', // User name from auth, updated at init()
+    _api_token: '', // api_token to be used. TODO: figure out how to transfer it
+    _empty_slot_str: "Empty slot", // moved out of func for localization purposes
+    _broadcast_slot: 0,
+
     init: function() {
-
-        //Initialise Accordion widget
-        ep.initAccordion();
-
         //get Platform info
-        $.get("/api/v1/info", function(data){
-            $("txt_wlcm_msg").innerHTML = data.welcome_message;
-            $("txt_vendor").innerHTML = data.vendor;
-            $("txt_sw_version").innerHTML = data.sw_version;
-            $("txt_hw_version").innerHTML = data.hw_version;
-            $("txt_api_versions").innerHTML = data.supported_api_versions;
-        });
+        $.get("/api/v1/info?format=json", function(platform_info){
+            ep.slots = platform_info.slots;
 
-        ep.updateModules();
+            ep.updateModuleList(); // manually update list of modules
 
-        //Set click handlers for Clear and Export buttons
-        $("#btn_clear").on('click', function() {
-            $('#log_console').empty();
+            // TODO: set websocket listener to update modules
+            // in websocket we expect slot id and module name
+
+            // TODO: update ep._username
+            // TODO: update ep._api_token
         });
     },
 
-    generateHeaderContainer: function(id, name) {
-        //display slot index
-        var result = "<h3 class = 'module_header' id='module_header" + id + "'>" + id + ". ";
-        if (name != null) {
-            //display module name
-            result += name;
-        } else {
-        result += "Empty slot";
+    scpi: function(slot_id, scpi_command, callback) {
+        $.post({
+            url: '/api/v1/send_scpi?format=json&slot='+slot_id,
+            data: scpi_command,
+            success: function(scpi_response) {
+                    if (callback != null) callback(scpi_response);
+                },
+            error: function(jqxhr, status, error) {
+                    // TODO: do something
+                    alert("Something went wrong, our elves work on this already" +
+                        " (not really). Meanwhile, keep in mind that " + error);
+                }
+        });
+    },
+
+    _module_template: function(slot_id, module_name) {
+        /* the only reason not to make this function inline is a use case with
+        dynamic extension of ports. If module is connected into non-standard
+        port, e.g. directly to free USB slot, or platform is not configured
+        correctly, new slot will be dynamically created above number specified
+        in platform info slots.
+        In future, we might consider switching to a normal template engine like
+        Mustache */
+        return "<header data-slot-id='" + slot_id + "' id='module_header_" +
+            slot_id + "'>" + slot_id + ".&nbsp;" + "<span id='module_name_" +
+            slot_id + "'>" + (module_name || ep._empty_slot_str) +
+            "</span></header>" + "<details id='module_control_panel_" +
+            slot_id + "'></details>";
+    },
+
+    updateModuleList: function() {  //manually update full list of modules
+        // create containers for modules by number of slots in this platform
+        var module_list_container = $('#modules_list');
+        module_list_container.empty();
+        // ep.slots + 1 because of Broadcast module in slot 0
+        for (var i=0; i<ep.slots+1; i++) {
+            module_list_container.append(
+                ep._module_template(i, ep._empty_slot_str));
         }
 
-        result = result + "</h3>";
-        return result;
-    },
+        // get list of modules and update created containers
+        $.get("/api/v1/modules_list?format=json", function(modules){
+            modules.forEach(function(module_name, slot_id) {
+                if (slot_id > ep.slots) {
+                    /* module plugged through standalone adapter or platform
+                    * ports are not configured - add new slot dynamically */
+                    module_list_container.append(
+                        ep._module_template(slot_id, ep._empty_slot_str));
+                }
+                // update module name in header
+                $("#module_name_" + slot_id).text(module_name || ep._empty_slot_str);
 
-    generateControlPanelContainer: function(id, name, used_by) {
-        return "<div id='control_panel" + id + "' class = 'disabled control_panel'></div>";
-    },
+                ep._updateModuleUI(slot_id, module_name);
 
-    addHeaderLockButton: function(id, used_by) {
-        var btn = $('<a/>', { id: "tgl_lock" + id});
+                // manually update lock status
+                if (module_name == null || slot_id==ep._broadcast_slot) {
+                    // Broadcast pseud
+                    ep._markUsedBy(slot_id, null);
+                }
+                else {
+                    ep._updateModuleLockStatus(slot_id, module_name);
+                }
 
-        $('#module_header' + id).append(btn);
+                // TODO: set websocket listener to monitor lock status
 
-        if (used_by != null) {
-            btn.addClass("down");
-            btn.text("Unlock");
-        } else {
-            btn.text("Lock");
-        }
-
-
-
-        btn.click(function() {
-
-            btn.toggleClass("down");
-            if (btn.hasClass("down")) {
-                //Lock the module
-                $.post("/api/v1/module/select?slot=" + id, function(data){
-                    writeToConsole("Locking module #" + id + ". Result: " + data + "\n");
-                });
-                btn.text("Unlock");
-
-            } else {
-
-                $.ajax({
-                    url: "/api/v1/module/select?slot=" + id,
-                    type: 'DELETE',
-                    success: function(data) {
-                        ep.writeToConsole("Removing lock from module #" + id + ". Result: " + data + "\n");
-                    }
-                });
-                btn.text("Lock");
-                $("#used_by" + id).text(" ");
-                $("#used_by" + id).hide();
-            }
-
-            //return false to block parent element's click handlers
-            return false;
-        });
-    },
-
-    addCommandSubmitPanel: function(id) {
-        var panel = "<div>SCPI command:</div><input id='scpi_command" + id + "' type='text'>";
-        panel += "<button id = 'send_scpi" + id + "'>Send</button>";
-        $("#control_panel" + id).append(panel);
-        $("#control_panel" + id).removeClass('disabled');
-        $('#send_scpi' + id).on('click', function() {
-            //Send SCPI command to the module
-            var command = $('#scpi_command' + id).val();
-            ep.writeToConsole("Sending command to module #" + id + ": " + command + "\n");
-            $.post("/api/v1/module?slot=" + id, command, function(data){
-                ep.writeToConsole("Response from module #" + id + ":\n" + data + "\n");
             });
         });
     },
 
-    initAccordion: function() {
-        $('#modules_list').accordion({
-            active: false,
-            collapsible:true,
+    _updateModuleUI: function(slot_id, module_name) {
+        /* TODO: replace with next sibling selector (less chance to be screwed
+        up with custom themes */
+        var control_panel = $("#module_control_panel_"+slot_id);
+        var header = $("#module_header_"+slot_id);
 
-            beforeActivate: function(event, ui) {
-                // The accordion believes a panel is being opened
-                var currHeader;
-                var currContent;
-                if (ui.newHeader[0]) {
-                    currHeader  = ui.newHeader;
-                    currContent = currHeader.next('.ui-accordion-content');
-                    // The accordion believes a panel is being closed
-                } else {
-                    currHeader  = ui.oldHeader;
-                    currContent = currHeader.next('.ui-accordion-content');
-                }
+        // DISABLE MODULE
+        // disable header click handler
+        header.off("click");
+        // collapse module control panel
+        control_panel.toggle(false).empty();
+        // mark module inactive
+        header.removeClass("active");
 
-                //Do not process click events if panel is disabled
-                if (currContent.hasClass('disabled')) {
-                    return false;
-                }
-            }
+        if (module_name == null) return;
+
+        // FROM THIS POINT ON, IT IS A REAL MODULE
+        // get module webUI based on config
+        $.getScript('/api/v1/module_ui_controls?format=json&slot='+slot_id)
+        .done(function(data, textStatus, xhr){
+            // add theme-specific handlers
+            control_panel.append("<input type='text' name='module_scpi_"
+                + slot_id + "'> <input type='button' value='Send SCPI' id=" +
+                "'module_raw_scpi_" + slot_id + "'>");
+            $("#module_raw_scpi_" + slot_id).click(function(){
+                // TODO: send SCPI command and provide callback
+                alert('Gotcha!');
+            });
+
+            // mark module active
+            header.addClass("active");
+            // set handler to toggle control panel
+            header.click(function(){
+                control_panel.toggle();
+                $.post({
+                    url: '/api/v1/lock_module?format=json&slot='+slot_id,
+                    success: function (lock_response) {
+                        if (lock_response != "OK")
+                            alert("Something went wrong. System didn't report" +
+                                "error, but we failed to acquire module lock." +
+                                "System response was: \n\n"+lock_response
+                                );
+                            else
+                                ep._markUsedBy(slot_id, ep._username);
+                        },
+                    error: function(XHR, status, error) {
+                        // TODO: alert user
+                        }
+                });
+            });
+        })
+        .fail(function(jqxhr, settings, exception){
+
         });
     },
 
-    writeToConsole: function (text) {
-        $('#log_console').append(text);
+    _updateModuleLockStatus: function(slot_id) {
+        if (slot_id == ep._broadcast_slot) {
+            // TODO: log warning
+        }
+        else {
+            $.get("/api/v1/lock_module?format=json&slot=" + slot_id,
+                function (username) {
+                    ep._markUsedBy(slot_id, username)
+                });
+        }
     },
 
-    updateModules: function() {
-        //get list of modules and their statuses
-        $.get("/api/v1/modules_list", function(data){
-            var num_of_modules = 0;
-            var used_modules = 0;
-            for (var i=0; i < data.length; i++) {
-                    var module = data[i];
-                    if (module[0] != null) {
-                        num_of_modules = num_of_modules + 1;
-                        if (module[1] != null) {
-                            used_modules = used_modules + 1;
-                        }
-                    }
-                    var header = ep.generateHeaderContainer(i, module[0]);
-                    var control_panel = ep.generateControlPanelContainer(i, module[0], module[1]);
-                    $('#modules_list').append(header + control_panel);
-                    if (i != 0) {
-                        //It's not a broadcast module
-                        if (module[0] != null) {
-                            //Slot is not empty
-                            ep.addHeaderLockButton(i, module[1]);
-                            ep.addCommandSubmitPanel(i);
-                        }
-                    } else {
-                        //It's a broadcast module
-                        ep.addCommandSubmitPanel(i);
-                    }
-                    $('#modules_list').accordion("refresh");
-            }
-
-            $("txt_number_of_slots").innerHTML = data.length;
-            $("txt_number_of_modules").innerHTML = num_of_modules;
-            $("txt_modules_in_use").innerHTML = used_modules;
-        });
+    _markUsedBy: function(slot_id, username) {
+        // TODO: add GUI
     }
+
 };
