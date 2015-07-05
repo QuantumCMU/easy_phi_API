4ea0f665f1991dc291b2849d1e5318a8e37d4228
Marat <valiev.m@gmail.com>
2015-07-05 23:53:25 +0300

    first stable version of new UI
    known issues:
    - lock status is not supported yet
    - missing console
    - HTML/CSS still to be improved
    - no auth
    - no api keys handling

--- static/js/main.js
+++ static/js/main.js
@@ -4,7 +4,7 @@ $(document).ready(function () {
     ep.init();
 });
 
-var ep = ep || {
+var ep = window['ep'] || {
     base_url: '',
     slots: 0,
     _username: 'Somebody', // User name from auth, updated at init()
@@ -12,7 +12,7 @@ var ep = ep || {
     _empty_slot_str: "Empty slot", // moved out of func for localization purposes
     _broadcast_slot: 0,
 
-    init: function() {
+    init: function(base_url) {
         // set global ajax error handler
         /*
         $(document).ajaxError(function(event, jqxhr, settings, error) {
@@ -22,6 +22,10 @@ var ep = ep || {
                "API call to url "+settings.url+" failed with error: " + error
            )
         }); */
+        if (base_url == null) {
+            base_url = window.location.protocol + "//" +window.location.host;
+        }
+        ep.base_url = base_url;
 
         //get Platform info
         $.get(ep.base_url + "/api/v1/info?format=json", function(platform_info){
@@ -42,11 +46,12 @@ var ep = ep || {
             scpi_command,
             function(scpi_response) {
                 if (callback != null) callback(scpi_response);
+                ep.log(slot_id, scpi_command, scpi_response);
             }
         );
     },
 
-    _module_template: function(slot_id, module_name) {
+    _add_module: function(container, slot_id, module_name) {
         /* the only reason not to make this function inline is a use case with
         dynamic extension of ports. If module is connected into non-standard
         port, e.g. directly to free USB slot, or platform is not configured
@@ -54,11 +59,21 @@ var ep = ep || {
         in platform info slots.
         In future, we might consider switching to a normal template engine like
         Mustache */
-        return "<header data-slot-id='" + slot_id + "' id='module_header_" +
-            slot_id + "'>" + slot_id + ".&nbsp;" + "<span id='module_name_" +
-            slot_id + "'>" + (module_name || ep._empty_slot_str) +
-            "</span></header>" + "<details id='module_control_panel_" +
-            slot_id + "'></details>";
+        container.append("<header id='module_header_"+slot_id+"'>"+slot_id+".&nbsp;" +
+            // module name
+            "<span class='module_name' id='module_name_"+slot_id+"'>"+
+            (module_name||ep._empty_slot_str)+"</span>" +
+            // module lock
+            "<span class='module_lock' id='modle_name_"+slot_id+"'>You</span>"+
+            // rest of header
+            "</header>" +
+            // control panel
+            "<section class='control_panel' id='module_control_panel_"+slot_id+
+            "'></section>");
+        $("#module_header_"+slot_id).click(function(){
+            if (!$(this).hasClass("active")) return;
+            $(this).toggleClass("open");
+        });
     },
 
     updateModuleList: function() {  //manually update full list of modules
@@ -66,10 +81,8 @@ var ep = ep || {
         var module_list_container = $('#modules_list');
         module_list_container.empty();
         // ep.slots + 1 because of Broadcast module in slot 0
-        for (var i=0; i<ep.slots+1; i++) {
-            module_list_container.append(
-                ep._module_template(i, ep._empty_slot_str));
-        }
+        for (var i=0; i<ep.slots+1; i++)
+            ep._add_module(module_list_container, i, ep._empty_slot_str);
 
         // get list of modules and update created containers
         $.get(ep.base_url+"/api/v1/modules_list?format=json", function(modules){
@@ -77,8 +90,7 @@ var ep = ep || {
                 if (slot_id > ep.slots) {
                     /* module plugged through standalone adapter or platform
                     * ports are not configured - add new slot dynamically */
-                    module_list_container.append(
-                        ep._module_template(slot_id, ep._empty_slot_str));
+                    ep._add_module(module_list_container, slot_id, ep._empty_slot_str);
                 }
                 // update module name in header
                 $("#module_name_" + slot_id).text(module_name || ep._empty_slot_str);
@@ -87,7 +99,7 @@ var ep = ep || {
 
                 // manually update lock status
                 if (module_name == null || slot_id==ep._broadcast_slot) {
-                    // Broadcast pseud
+                    // Broadcast pseudo module
                     ep._markUsedBy(slot_id, null);
                 }
                 else {
@@ -107,10 +119,8 @@ var ep = ep || {
         var header = $("#module_header_"+slot_id);
 
         // DISABLE MODULE
-        // disable header click handler
-        header.off("click");
         // collapse module control panel
-        control_panel.toggle(false).empty();
+        control_panel.empty();
         // mark module inactive
         header.removeClass("active");
 
@@ -120,33 +130,10 @@ var ep = ep || {
         // get module webUI based on config
         var widgets_script_url = ep.base_url +
             '/api/v1/module_ui_controls?format=json&slot=' +
-            slot_id + '&container=%23module_control_panel_'+slot_id
-        $.getScript(widgets_script_url)
-        .done(function(data, textStatus, xhr){
-            // add theme-specific handlers
-            control_panel.append("<input type='text' name='module_scpi_"
-                + slot_id + "'> <input type='button' value='Send SCPI' id=" +
-                "'module_raw_scpi_" + slot_id + "'>");
-            $("#module_raw_scpi_" + slot_id).click(function(){
-                // TODO: send SCPI command and provide callback
-                alert('Gotcha!');
-            });
-
+            slot_id + '&container=%23module_control_panel_'+slot_id;
+        $.getScript(widgets_script_url).done(function(){
             // mark module active
             header.addClass("active");
-            // set handler to toggle control panel
-            header.click(function() {
-                control_panel.toggle();
-                if (slot_id != ep._broadcast_slot) { $.post(
-                    ep.base_url+'/api/v1/lock_module?format=json&slot='+slot_id,
-                    function (lock_response) {
-                        ep._markUsedBy(slot_id, ep._username);
-                    });
-                }
-            });
-        })
-        .fail(function(jqxhr, settings, exception){
-
         });
     },
 
@@ -164,6 +151,18 @@ var ep = ep || {
 
     _markUsedBy: function(slot_id, username) {
         // TODO: add GUI
-    }
+    },
 
+    log: function(slot_id, scpi_command, scpi_response) {
+        if (scpi_response == null) { // log(scpi_command, scpi_response)
+            scpi_response = scpi_command;
+            scpi_command = slot_id;
+            slot_id = null;
+            if (scpi_response == null) { // log(scpi_response)
+                scpi_response = scpi_command;
+                scpi_command = null;
+            }
+        }
+        alert(scpi_response);
+    }
 };
